5d8cb2039b2823334df29c27a98112c43da26d44
MargueriteT <71390259+MargueriteT@users.noreply.github.com>
2021-10-15 10:37:00 +0200

    Update settings.py

--- LitProject/LitProject/settings.py
+++ LitProject/LitProject/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7w&11^2fp+nyprfrq4%u4)x8le3uhnne6n=*^g6ybeaq5zt57b'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

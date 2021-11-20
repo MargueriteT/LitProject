e9c07e35c4c3933215e38881c3f3273462fd52bd
Marguerite Teulon <marguerite.teulon@gmail.com>
2021-11-20 13:17:15 +0100

    Update settings.py

--- LitProject/LitProject/settings.py
+++ LitProject/LitProject/settings.py
@@ -12,15 +12,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from .configuration import SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7w&11^2fp+nyprfrq4%u4)x8le3uhnne6n=*^g6ybeaq5zt57b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
